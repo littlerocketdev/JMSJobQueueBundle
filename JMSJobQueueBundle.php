f43e371d12d0bafeeac24d7199d871cd09b0c2b2
Daan Gebraad <daan@littlerocket.nl>
2023-12-19 13:08:45 +0100

    5969243 fix(di): adjust bundle configuration

--- JMSJobQueueBundle.php
+++ JMSJobQueueBundle.php
@@ -18,8 +18,6 @@
 
 namespace JMS\JobQueueBundle;
 
-use JMS\JobQueueBundle\DependencyInjection\CompilerPass\JobSchedulersPass;
-use JMS\JobQueueBundle\DependencyInjection\CompilerPass\LinkGeneratorsPass;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
@@ -27,6 +25,5 @@ class JMSJobQueueBundle extends Bundle
 {
     public function build(ContainerBuilder $container)
     {
-        $container->addCompilerPass(new LinkGeneratorsPass());
     }
 }
